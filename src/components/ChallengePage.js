006a9ba1bed3e2c0b077faf9e80d98a0be9cbd6b
Mukul Jain <jainmukul1996@gmail.com>
2017-07-04 03:37:00 +0530

    Challenge page structure

--- src/components/ChallengePage.js
+++ src/components/ChallengePage.js
@@ -9,8 +9,32 @@ class ChallengePage extends React.Component {
     render() {
 
         return (
-            <div>
-                <h1>Challenge Page</h1>
+            <div className="challenge-page">
+                <a className="row align-middle back-to-challenge">
+                    <img className="columns shrink" src={require('../media/back-arrow.svg')}/>
+                    <h4 className="columns shrink">Back to Challenges</h4>
+                </a>
+
+                <div className="row">
+                    <div className="columns">
+                        <div className="default-card">
+                            <h2>Challenge Header</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="columns large-8">
+                        <div className="default-card">
+                            <h2>Challenge Details</h2>
+                        </div>
+                    </div>
+                    <div className="columns large-4">
+                        <div className="default-card">
+                            <h2>Challenge Repo</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
