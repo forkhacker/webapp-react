ffeab6d981dde2863b4e9f1a553f848baeef97f2
Uttpal <uttpal25993@gmail.com>
2017-10-07 17:02:55 +0530

    add get cahllenge actions

--- src/actions/challengeActions.js
+++ src/actions/challengeActions.js
@@ -9,14 +9,27 @@ export function challengesListSuccess(challenges) {
         challenges,
     };
 }
+export function challengesGetSuccess(challengeData) {
+    return {
+        type : types.CHALLENGES_GET_SUCCESS,
+        challenge: challengeData,
+    };
+}
 
 export function listChallenges() {
     return async dispatch => {
         // dispatch(loaderActions.show());
         const challenges = await challenge.list();
         console.log(challenges);
-        debugger;
         dispatch(challengesListSuccess(challenges));
         // return dispatch(loaderActions.hide());
+    };
+}
+
+export function getChallenge(id) {
+    return async dispatch => {
+        const challengeData = await challenge.get(id);
+        console.log(challengeData);
+        dispatch(challengesGetSuccess(challengeData));
     };
 }
